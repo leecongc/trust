15f4788a3fc8aac044e577cb7510510b56cb1800
Leemc <11623112@qq.com>
2018-01-17 17:33:41 +0800

    创建项目、完成首页布局（首页《新闻资讯》样式未编写完成）

---
+++ pages/index/index.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true, /*是否显示面板指示点*/
+    autoplay: true,      /*是否自动切换*/
+    interval: 5000,       /*自动切换时间间隔*/
+    duration: 1000        /*滑动动画时长*/
+  }
+})
